e25a2b13963ce62e033e664d60aad4735c7b75a6
Andrii Savchuk <30691415+savchukidze@users.noreply.github.com>
2018-09-25 17:51:54 +0300

    skip null, bill's numbers
    skip null value and resolve problem with the identical bill's numbers

--- R/voting_mps.R
+++ R/voting_mps.R
@@ -9,57 +9,58 @@
 #' @export
 
 voting_mps <- function (id = 4896, key)
-
+   
 {
    voting <- data.frame()
-
+   
    for (i in id) {
       request <- httr::GET(url = "https://rada4you.org/",
                            path = paste0("api/v1/divisions//", i, ".json"),
                            query = list(key = key))
-
+      
       if (httr::status_code(request) != 200) {
          stop(message(paste("Помилка. Код відповіді сервера: "),
                       httr::status_code(request)))
       }
-
+      
       else
-
-         {
+         
+      {
          response <- httr::content(request, as = "text")
          json <- jsonlite::fromJSON(response)
-
+         
          print(json$id)
-
+         
+       #if null
          null <- function(x) {
             if (is.null(x))
                return("NA")
             return(x)
-
+            
          }
-
+         
          votes <- data.frame(
-                           vote_id = json$id,
-                           vote_name = json$name,
-                           bill_id = null(json$bills$id),
-                           bill_number = null(paste0("№", json$bills$official_id)),
-                           bill_title = null(json$bills$title),
-                           date = json$date, mp_id = json$votes$member$person$id,
-                           mp_name = paste(json$votes$member$first_name, json$votes$member$last_name),
-                           party = json$votes$member$party,
-                           electorate = json$votes$member$electorate,
-                           vote = null(json$votes$vote),
-
-                                                    stringsAsFactors = F)
-
+            vote_id = json$id,
+            vote_name = json$name,
+            bill_id = null(unique(json$bills$id)),
+            bill_number = null(unique(paste0("№", json$bills$official_id))),
+            bill_title = null(unique(json$bills$title)),
+            date = json$date, mp_id = json$votes$member$person$id,
+            mp_name = paste(json$votes$member$first_name, json$votes$member$last_name),
+            party = json$votes$member$party,
+            electorate = json$votes$member$electorate,
+            vote = json$votes$vote,
+            
+                        stringsAsFactors = F)
+         
          voting <- rbind.data.frame(voting, votes)
          voting$date <- as.Date(voting$date)
-
-         }
-
+         
+      }
+      
       library(magrittr)
       library(stringr)
-
+      
       voting$party <- voting$party %>%
          str_replace_all(pattern = "Фракція Політичної партії \"НАРОДНИЙ ФРОНТ\"",
                          replacement = "Народний фронт") %>%
@@ -77,14 +78,14 @@ voting_mps <- function (id = 4896, key)
                          replacement = "група \"Відродження\"") %>%
          str_replace_all(pattern = "Фракція Політичної партії \"Опозиційний блок\" у Верховній Раді України восьмого скликання",
                          replacement = "Опозиційний блок")
-
+      
       Sys.sleep(1)
-
+      
       detach(package:magrittr)
       detach(package:stringr)
-
+      
    }
-
+   
    return(voting)
-
+   
 }

a077e152028551575fd466d65e4360624cee25ba
Andrii Savchuk <30691415+savchukidze@users.noreply.github.com>
2019-12-03 13:42:22 +0200

    Update mps_info.R

--- R/mps_info.R
+++ R/mps_info.R
@@ -64,6 +64,21 @@ mps_info <- function (id, key)
             str_replace_all(pattern = "Фракція Політичної партії \"Опозиційний блок\" у Верховній Раді України восьмого скликання",
                             replacement = "Опозиційний блок")
 
+         
+            #9скликання
+         depbioall$party <- depbioall$party %>%
+   str_replace_all(pattern = "депутатська група За майбутнє", 
+                   replacement = 'За майбутнє') %>% 
+   str_replace_all(pattern = "фракція Батьківщина", 
+                   replacement = 'Батьківщина') %>% 
+   str_replace_all(pattern = "фракція Голос", 
+                   replacement = 'Голос') %>% 
+   str_replace_all(pattern = "фракція Європейська солідарність", 
+                   replacement = 'ЄС') %>% 
+   str_replace_all(pattern = "фракція Опозиційна платформа - За життя", 
+                   replacement = 'ОПЗЖ') %>% 
+   str_replace_all(pattern = "фракція Слуга народу", 
+                   replacement = 'Слуга народу')
          Sys.sleep(1)
 
          detach(package:magrittr)
